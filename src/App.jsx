64016c8d909980ee698ee76cf406f106edefdd0d
Ankan Das <127923458+ankan20@users.noreply.github.com>
2024-03-10 17:29:09 +0530

    Todo with redux toll kit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Todo from './components/Todo'
+import AddTodo from './components/AddTodo'
+
+function App() {
+  
+
+  return (
+    <>
+      Redux Tool -Kit 
+      <AddTodo/>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
